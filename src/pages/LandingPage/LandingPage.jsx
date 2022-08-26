154bfaab02b42a823178fd4582b0f6102decce33
ArlessonMoura <arlessonmss@gmail.com>
2022-08-26 12:37:19 -0300

    1 e 4 sections with corner elements

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,8 @@ import React, { Component  } from 'react';
 import Header from '../../components/Header/Header';
 import cornerTopL from '../../assets/images/left-top-big.png';
 import cornerTopR from '../../assets/images/right-top-big.png';
+import cornerBottomL from '../../assets/images/left-bottom-big.png';
+import cornerBottomR from '../../assets/images/right-bottom-big.png';
 import './style.scss';
 
 class LandingPage extends Component { 
@@ -12,10 +14,10 @@ class LandingPage extends Component {
         <Header/>
         <main>    
           <section id="section-one">
-            <img src={ cornerTopL } alt="left-top-corner" />
+            <img className="col-2" src={ cornerTopL } alt="left-top-corner" />
             <p>Hello World!</p>
             <h1>Segmento 1</h1>
-            <img src={ cornerTopR } alt="left-top-corner" />
+            <img className="col-2" src={ cornerTopR } alt="right-top-corner" />
           </section>
           <section id="section-two">
             <p>Hello World!</p>
@@ -26,8 +28,10 @@ class LandingPage extends Component {
             <h1>Segmento 3</h1>
           </section>
           <section id="section-four">
+            <img className="col-2" src={ cornerBottomL } alt="left-bottom-corner" />
             <p>Hello World!</p>
             <h1>Segmento 4</h1>
+            <img className="col-2" src={ cornerBottomR } alt="right-bottom-corner" />
           </section>
         </main>    
       </>
