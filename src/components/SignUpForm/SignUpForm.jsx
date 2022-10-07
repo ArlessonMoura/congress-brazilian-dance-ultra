1aac0e070a3a5b78f47eef703d648d589320f6cd
ArlessonMoura <arlessonmss@gmail.com>
2022-10-07 19:42:04 -0300

    LGPD call and Form adjusts

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -3,6 +3,7 @@ import {
   Form, FormGroup, FloatingLabel, Button,
   Row, Col,
 } from 'react-bootstrap';
+import ModalLGPD from '../ModalLGPD/ModalLGPD';
 import './style.scss';
 
 class SignUpForm extends Component {
@@ -17,8 +18,10 @@ class SignUpForm extends Component {
       password: '',
       password2: '',
       accepted: false,
+      privacyPolice: false,
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.rulesModal = this.rulesModal.bind(this);
   }
 
   handleInputChange(event) {
@@ -31,65 +34,75 @@ class SignUpForm extends Component {
     });
   }
 
+  rulesModal(bool) {
+    this.setState({ privacyPolice: bool });
+  }
+
   render() {
     const {
-      name, cpf, birth, email, phone, password, password2, accepted,
+      name, cpf, birth, email, phone, password, password2, accepted, privacyPolice,
     } = this.state;
+
     return (
-      <Form>
+      <Form className="sign-up">
+        <Button title="Politica de Privacidade" variant="Link" size="lg" onClick={() => this.rulesModal(true)}>
+          <i className="bi bi-question-diamond-fill" />
+        </Button>
         <Row>
           <FormGroup className="mb-3" controlId="formName">
             <FloatingLabel label="NOME COMPLETO">
-              <Form.Control onChange={this.handleInputChange} name="name" value={name} required type="text" placeholder="nome completo" />
+              <Form.Control size="lg" onChange={this.handleInputChange} name="name" value={name} required type="text" placeholder="nome completo" />
             </FloatingLabel>
           </FormGroup>
         </Row>
 
         <Row>
-          <Col>
+          <Col xs={12} sm={12} lg>
             <FormGroup className="mb-3" controlId="formCPF">
               <FloatingLabel label="CPF">
-                <Form.Control onChange={this.handleInputChange} name="cpf" value={cpf} required type="number" placeholder="CPF" pattern="" />
+                <Form.Control size="lg" onChange={this.handleInputChange} name="cpf" value={cpf} required type="number" placeholder="CPF" pattern="" />
               </FloatingLabel>
             </FormGroup>
           </Col>
 
-          <Col>
+          <Col xs={12} sm={12} lg>
             <FormGroup className="mb-3" controlId="formBirth">
               <FloatingLabel label="DATA DE NASCIMENTO">
-                <Form.Control onChange={this.handleInputChange} name="birth" value={birth} required type="date" placeholder="Data de Nascimento" />
+                <Form.Control size="lg" onChange={this.handleInputChange} name="birth" value={birth} required type="date" placeholder="Data de Nascimento" />
               </FloatingLabel>
             </FormGroup>
           </Col>
         </Row>
+
         <Row>
           <FormGroup className="mb-3" controlId="formEmail">
             <FloatingLabel label="E-MAIL">
-              <Form.Control onChange={this.handleInputChange} name="email" value={email} required type="email" placeholder="e-mail" />
+              <Form.Control size="lg" onChange={this.handleInputChange} name="email" value={email} required type="email" placeholder="e-mail" />
             </FloatingLabel>
           </FormGroup>
         </Row>
+
         <Row>
-          <Col>
+          <Col xs={12} sm={12} lg>
             <FormGroup className="mb-3" controlId="formPhone">
               <FloatingLabel label="TELEFONE">
-                <Form.Control onChange={this.handleInputChange} name="phone" value={phone} required type="phone" placeholder="Telefone" />
+                <Form.Control size="lg" onChange={this.handleInputChange} name="phone" value={phone} required type="phone" placeholder="Telefone" />
               </FloatingLabel>
             </FormGroup>
           </Col>
 
-          <Col>
+          <Col xs={12} sm={12} lg>
             <FormGroup className="mb-3" controlId="formPassword">
               <FloatingLabel label="SENHA">
-                <Form.Control onChange={this.handleInputChange} name="password" value={password} required type="password" placeholder="Senha" />
+                <Form.Control size="lg" onChange={this.handleInputChange} name="password" value={password} required type="password" placeholder="Senha" />
               </FloatingLabel>
             </FormGroup>
           </Col>
 
-          <Col>
+          <Col xs={12} sm={12} lg>
             <FormGroup className="mb-3" controlId="formConfirmPassword">
               <FloatingLabel label="CONFIRME SENHA">
-                <Form.Control onChange={this.handleInputChange} name="password2" value={password2} required type="password" placeholder="Confirme Senha" />
+                <Form.Control size="lg" onChange={this.handleInputChange} name="password2" value={password2} required type="password" placeholder="Confirme Senha" />
               </FloatingLabel>
             </FormGroup>
           </Col>
@@ -97,13 +110,14 @@ class SignUpForm extends Component {
 
         <Row>
           <Col>
-            <Form.Check
-              className="mb-3"
-              name="accepted"
-              value={accepted}
-              required
-              label="Eu concordo com os termos e condições da Política de Privacidade"
-            />
+            <FormGroup>
+              <Form.Check
+                name="accepted"
+                value={accepted}
+                required
+                label="Eu concordo com os termos e condições da Política de Privacidade."
+              />
+            </FormGroup>
           </Col>
         </Row>
 
@@ -114,6 +128,7 @@ class SignUpForm extends Component {
             </Button>
           </div>
         </Row>
+        <ModalLGPD rulesModal={this.rulesModal} privacyPolice={privacyPolice} />
       </Form>
     );
   }
