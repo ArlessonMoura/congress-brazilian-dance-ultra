dd29cbbd7ffb7135853f79cf3b28f4f221550f9c
ArlessonMoura <arlessonmss@gmail.com>
2022-10-07 19:39:52 -0300

    section 3 and 4

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import Header from '../../components/Header/Header';
 import Languages from '../../components/LangMenu/LangMenu';
 import ShareModal from '../../components/ShareModal/ShareModal';
+import ScheduleCongress from '../../components/ScheduleCongress/ScheduleCongress';
+import Track from '../../components/Track/Track';
 import cornerTopL from '../../assets/images/left-top-big.png';
 import cornerTopR from '../../assets/images/right-top-big.png';
 import cornerBottomL from '../../assets/images/left-bottom-big.png';
 import cornerBottomR from '../../assets/images/right-bottom-big.png';
-import opening from '../../assets/images/opening-2023.png';
+import smallIconL from '../../assets/images/left-top-small.png';
+import smallIconR from '../../assets/images/right-top-small.png';
+import opening from '../../assets/images/opening2023.png';
 import whatsappIcon from '../../assets/images/whatsapp.png';
 import instagramIcon from '../../assets/images/instagram.png';
+import facebookIcon from '../../assets/images/facebook.png';
 import shareIcon from '../../assets/images/share.png';
 import './style.scss';
 
@@ -34,11 +40,11 @@ class LandingPage extends Component {
         <Header />
         <main>
           <Languages />
-          <article>
-            <section id="section-one" className="parallax">
+          <article className="major-main">
+            <section id="section-one" className="main-splits parallax">
 
-              <div className="corners col-2">
-                <img src={cornerTopL} alt="left-top-corner" />
+              <div className="d-flex align-items-start col-2">
+                <img className="corners" src={cornerTopL} alt="left-top-corner" />
               </div>
 
               <div className="opening col-8">
@@ -49,49 +55,100 @@ class LandingPage extends Component {
                 </div>
               </div>
 
-              <div className="corners col-2">
-                <img src={cornerTopR} alt="right-top-corner" />
+              <div className="d-flex flex-row-reverse align-items-start col-2">
+                <img className="corners" src={cornerTopR} alt="right-top-corner" />
               </div>
 
             </section>
 
-            <section id="section-two">
+            <section className="main-splits" id="section-two">
               <p>Hello World!</p>
               <h1>Segmento 2</h1>
             </section>
 
-            <section id="section-three">
-              <p>Hello World!</p>
-              <h1>Segmento 3</h1>
+            <section className="d-flex main-splits" id="section-three">
+
+              <div className="d-flex align-items-center col-2">
+                <img className="corners" src={smallIconL} alt="left-top-corner" />
+              </div>
+
+              <div className="d-flex flex-column align-content-center justify-content-center col-8">
+                <Tabs
+                  className="mb-3"
+                  defaultActiveKey="schedule"
+                  justify
+                >
+                  <Tab eventKey="attractions" title="Atrações">
+                    <ScheduleCongress />
+                  </Tab>
+                  <Tab eventKey="schedule" title="Cronograma">
+                    <ScheduleCongress />
+                  </Tab>
+                  <Tab eventKey="map" title="Como chegar">
+                    <Track />
+                  </Tab>
+                </Tabs>
+              </div>
+
+              <div className="d-flex flex-row-reverse align-items-center col-2">
+                <img className="corners" src={smallIconR} alt="right-top-corner" />
+              </div>
+
             </section>
 
-            <section id="section-four">
-              <img className="col-2" src={cornerBottomL} alt="left-bottom-corner" />
-              <div className="form col-8">
+            <section className="main-splits" id="section-four">
+
+              <div className="d-flex align-items-end col-2">
+                <img className="corners" src={cornerBottomL} alt="left-bottom-corner" />
+              </div>
+
+              <div className="subscription col-8">
+
                 <SignUpForm />
 
-                <div className="d-flex justify-content-center contacts">
-                  <a
-                    className="col-4"
-                    href="https://api.whatsapp.com/send?phone=5581996694649&text=Ol%C3%A1%2C%20estou%20interessado%20em%20me%20inscrever%20no%20%22Forr%C3%B3%20do%20Ultra%22!!!"
-                    target="_blank"
-                    rel="external noreferrer"
-                  >
-                    <input className="contact-icons" type="image" src={whatsappIcon} alt="whatsapp-icon" />
-                  </a>
-                  <a className="col-4" href="https://www.instagram.com/ultract/" target="_blank" rel="external noreferrer">
-                    <input className="contact-icons" type="image" src={instagramIcon} alt="instagram-icon" />
-                  </a>
+                <nav className="d-flex flex-column align-items-center web-social">
 
-                  <input onClick={() => this.showShareModal(true)} className="contact-icons" type="image" src={shareIcon} alt="share-icon" />
-                  <ShareModal
-                    showShareModal={this.showShareModal}
-                    isModalShareOn={isModalShareOn}
-                  />
-                </div>
+                  <div className="d-flex flex-column align-items-center contacts">
+                    <h2>Fale com um representante</h2>
+                    <div className="links">
+                      <a
+                        href="https://api.whatsapp.com/send?phone=5581996694649&text=Ol%C3%A1%2C%20estou%20interessado%20em%20me%20inscrever%20no%20%22Forr%C3%B3%20do%20Ultra%22!!!"
+                        target="_blank"
+                        rel="external noreferrer"
+                      >
+                        <input className="contact-icons" type="image" src={whatsappIcon} alt="whatsapp-icon" />
+                      </a>
 
+                      <a href="https://www.instagram.com/ultract/" target="_blank" rel="external noreferrer">
+                        <input className="contact-icons" type="image" src={instagramIcon} alt="instagram-icon" />
+                      </a>
+
+                      <a href="https://web.facebook.com/UltraCTBR" target="_blank" rel="external noreferrer">
+                        <input className="contact-icons" type="image" src={facebookIcon} alt="facebook-icon" />
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="d-flex flex-column align-items-center share">
+                    <h2>Compartilhe nas redes</h2>
+                    <input
+                      className="contact-icons"
+                      onClick={() => this.showShareModal(true)}
+                      type="image"
+                      src={shareIcon}
+                      alt="share-icon"
+                    />
+                    <ShareModal
+                      showShareModal={this.showShareModal}
+                      isModalShareOn={isModalShareOn}
+                    />
+                  </div>
+                </nav>
               </div>
-              <img className="col-2" src={cornerBottomR} alt="right-bottom-corner" />
+
+              <div className="d-flex flex-row-reverse align-items-end col-2">
+                <img className="corners" src={cornerBottomR} alt="right-bottom-corner" />
+              </div>
             </section>
           </article>
         </main>
