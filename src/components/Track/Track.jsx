4a50901a88009d440fec52ea0a70ce1bbd29cc3e
ArlessonMoura <arlessonmss@gmail.com>
2022-10-14 16:34:37 -0300

    iframe prop deprecated delete

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -9,7 +9,7 @@ function Track() {
     <Container fluid>
       <Row>
         <Col className="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.6585228510858!2d-35.01564768460291!3d-7.930687781171226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1058b2aecfdd%3A0x40b18be6a704b199!2sHotel%20Campestre%20de%20Aldeia!5e0!3m2!1spt-BR!2sbr!4v1665174238847!5m2!1spt-BR!2sbr" title="map" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.6585228510858!2d-35.01564768460291!3d-7.930687781171226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1058b2aecfdd%3A0x40b18be6a704b199!2sHotel%20Campestre%20de%20Aldeia!5e0!3m2!1spt-BR!2sbr!4v1665174238847!5m2!1spt-BR!2sbr" title="map" style={{ border: 0 }} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
         </Col>
       </Row>
       <Row>
