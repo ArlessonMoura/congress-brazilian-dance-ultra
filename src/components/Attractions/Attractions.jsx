7a67e013e7c321bb974ea2168c0a3cdf4ec4f83f
ArlessonMoura <arlessonmss@gmail.com>
2022-10-14 16:32:56 -0300

    create carousel

--- src/components/Attractions/Attractions.jsx
+++ src/components/Attractions/Attractions.jsx
@@ -1,35 +1,68 @@
 import React from 'react';
-
-// import test4 from '../../assets/images/test4.jpeg';
-// import test5 from '../../assets/images/test5.jpeg';
-// import test6 from '../../assets/images/test6.jpeg';
-// import test7 from '../../assets/images/test7.jpeg';
+import { Carousel } from 'react-bootstrap';
+import wtp from '../../assets/images/wtp.png';
+import attraction1 from '../../assets/images/attraction1.png';
+import attraction2 from '../../assets/images/attraction2.png';
+import attraction3 from '../../assets/images/attraction3.png';
 import './style.scss';
 
 function Attractions() {
   return (
+    <Carousel indicators="false" pause="hover" variant="dark">
+      <Carousel.Item>
+        <img
+          className="size-img d-block"
+          src={wtp}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h2>Quais serão os professores e competidores desse ano?</h2>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-    <div className="test">
-      <h1>pessoas</h1>
-    </div>
+      <Carousel.Item>
+        <img
+          className="size-img d-block"
+          src={attraction1}
+          alt="Second slide"
+        />
 
-  // <Carousel variant="dark">
-  //   <Carousel.Item>
-  //     <Image src={test4} alt="First slide" />
-  //   </Carousel.Item>
+        <Carousel.Caption>
+          <h2>Celio Rocha</h2>
+          <h3>Aqui é claro que é presença garantida!</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-  //   <Carousel.Item>
-  //     <Image src={test5} alt="Second slide" />
-  //   </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="size-img d-block"
+          src={attraction2}
+          alt="Third slide"
+        />
 
-  //   <Carousel.Item>
-  //     <Image src={test6} alt="Third slide" />
-  //   </Carousel.Item>
+        <Carousel.Caption>
+          <h2>Fábia & Erisson</h2>
+          <h3>
+            Também presença mais que garntida!
+          </h3>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-  //   <Carousel.Item>
-  //     <Image src={test7} alt="Third slide" />
-  //   </Carousel.Item>
-  // </Carousel>
+      <Carousel.Item>
+        <img
+          className="size-img d-block"
+          src={attraction3}
+          alt="Fourth slide"
+        />
+
+        <Carousel.Caption>
+          <h2>Erica & Witheney</h2>
+          <h3>
+            E esse casal fenomenal, será que virão para tentar o Bi-campeonato!
+          </h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
