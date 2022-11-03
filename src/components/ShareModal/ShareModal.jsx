5e51a7769804dc4913fb148cdc64a39b36a3b875
ArlessonMoura <arlessonmss@gmail.com>
2022-11-03 18:06:07 -0300

    alert conflict

--- src/components/ShareModal/ShareModal.jsx
+++ src/components/ShareModal/ShareModal.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Button, Modal, Form, InputGroup, Row, Col,
+  Button, Modal, Form, InputGroup, Row, Col, Alert,
 } from 'react-bootstrap';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import PropTypes from 'prop-types';
@@ -14,18 +14,27 @@ class ShareModal extends Component {
     super(props);
     this.state = {
       url: 'https://www.instagram.com/ultract/',
+      copyMsg: false,
     };
+    this.onHideTriggers = this.onHideTriggers.bind(this);
+  }
+
+  onHideTriggers() {
+    const { showShareModal } = this.props;
+    this.setState({ copyMsg: false });
+    showShareModal(false);
   }
 
   render() {
-    const { isModalShareOn, showShareModal } = this.props;
-    const { url } = this.state;
+    const { isModalShareOn } = this.props;
+    const { url, copyMsg } = this.state;
 
     return (
       <Modal
         show={isModalShareOn}
-        onHide={() => showShareModal(false)}
+        onHide={this.onHideTriggers}
         centered
+        size="lg"
       >
         <Modal.Header closeButton>
           <Modal.Title>
@@ -35,7 +44,7 @@ class ShareModal extends Component {
 
         <Modal.Body>
           <Row>
-            <Col>
+            <Col className="d-flex justify-content-center">
               <a
                 target="_blank"
                 href="https://www.facebook.com/sharer/sharer.php?u=https://www.instagram.com/ultract/"
@@ -43,6 +52,8 @@ class ShareModal extends Component {
               >
                 <input type="image" className="share" src={facebookIcon} alt="Whatsapp" />
               </a>
+            </Col>
+            <Col className="d-flex justify-content-center">
               <a
                 target="_blank"
                 href="https://api.whatsapp.com/send?text=https://www.instagram.com/ultract/"
@@ -55,9 +66,12 @@ class ShareModal extends Component {
 
           <Row>
             <Col>
+              <Alert show={copyMsg} variant="primary">
+                Endereço do site copiado com sucesso!
+              </Alert>
               <InputGroup>
-                <CopyToClipboard text={url} onCopy={() => alert('Endereço do site copiado com sucesso!')}>
-                  <InputGroup.Text onClick={this.copyURL}>
+                <CopyToClipboard text={url} onCopy={() => this.setState({ copyMsg: true })}>
+                  <InputGroup.Text>
                     <Button variant="light">
                       <Translator path="modalShare.btnCopy" />
                     </Button>
